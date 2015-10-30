586b2860e382349056c35d7a69b3962d8f2ca295
Raman Prasad <raman_prasad@harvard.edu>
2015-10-30 16:26:05 -0400

    add predict migrations

---
+++ gentb_website/tb_website/apps/predict/migrations/0001_initial.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+import model_utils.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('tb_users', '0003_auto_20150820_1345'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='DatasetScriptRun',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', model_utils.fields.AutoCreatedField(default=django.utils.timezone.now, verbose_name='created', editable=False)),
+                ('modified', model_utils.fields.AutoLastModifiedField(default=django.utils.timezone.now, verbose_name='modified', editable=False)),
+                ('notes', models.TextField(blank=True)),
+                ('result_received', models.BooleanField(default=False)),
+                ('result_success', models.BooleanField(default=False)),
+                ('result_data', models.TextField(blank=True)),
+                ('md5', models.CharField(help_text=b'auto-filled on save', max_length=40, db_index=True, blank=True)),
+            ],
+            options={
+                'ordering': ('-modified', '-created'),
+            },
+        ),
+        migrations.CreateModel(
+            name='PredictDataset',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', model_utils.fields.AutoCreatedField(default=django.utils.timezone.now, verbose_name='created', editable=False)),
+                ('modified', model_utils.fields.AutoLastModifiedField(default=django.utils.timezone.now, verbose_name='modified', editable=False)),
+                ('title', models.CharField(max_length=255, verbose_name=b'Dataset title')),
+                ('dropbox_url', models.URLField()),
+                ('description', models.TextField(verbose_name=b'Dataset description')),
+                ('file_directory', models.CharField(max_length=255, blank=True)),
+                ('has_prediction', models.BooleanField(default=False)),
+                ('md5', models.CharField(help_text=b'auto-filled on save', max_length=40, db_index=True, blank=True)),
+            ],
+            options={
+                'ordering': ('-created', 'title'),
+            },
+        ),
+        migrations.CreateModel(
+            name='PredictDatasetFile',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', model_utils.fields.AutoCreatedField(default=django.utils.timezone.now, verbose_name='created', editable=False)),
+                ('modified', model_utils.fields.AutoLastModifiedField(default=django.utils.timezone.now, verbose_name='modified', editable=False)),
+                ('name', models.CharField(help_text=b'Name of the file (w/o) the path', max_length=255)),
+                ('fullpath', models.TextField(help_text=b'Full path to the file')),
+                ('size', models.IntegerField(default=0, help_text=b'Size of the file in bytes')),
+                ('dataset', models.ForeignKey(to='predict.PredictDataset')),
+            ],
+            options={
+                'ordering': ('-created', 'dataset', 'name'),
+            },
+        ),
+        migrations.CreateModel(
+            name='PredictDatasetNote',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', model_utils.fields.AutoCreatedField(default=django.utils.timezone.now, verbose_name='created', editable=False)),
+                ('modified', model_utils.fields.AutoLastModifiedField(default=django.utils.timezone.now, verbose_name='modified', editable=False)),
+                ('title', models.CharField(max_length=255)),
+                ('note', models.TextField()),
+                ('dataset', models.ForeignKey(to='predict.PredictDataset')),
+            ],
+            options={
+                'ordering': ('-modified', '-created'),
+            },
+        ),
+        migrations.CreateModel(
+            name='PredictDatasetStatus',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=50)),
+                ('slug', models.SlugField(blank=True)),
+                ('sort_order', models.IntegerField()),
+            ],
+            options={
+                'ordering': ('sort_order', '-name'),
+                'verbose_name_plural': 'Predict Dataset Statuses',
+            },
+        ),
+        migrations.CreateModel(
+            name='ScriptToRun',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', model_utils.fields.AutoCreatedField(default=django.utils.timezone.now, verbose_name='created', editable=False)),
+                ('modified', model_utils.fields.AutoLastModifiedField(default=django.utils.timezone.now, verbose_name='modified', editable=False)),
+                ('name', models.CharField(max_length=100)),
+                ('is_chosen_script', models.BooleanField(default=True)),
+                ('script', models.TextField(help_text=b'Example of JSON argument: \'{"admin_url": "http://127.0.0.1:8000/tb-admin/predict/PredictDataset/3/", "callback_url": "some_url to receive results", "dataset_id": 3, "user_email": "user_who_uploaded_file@place.edu", "file1_path": ".../tb_uploaded_files/shared-files/2015/08/Predict_-_genTB_BnVjFcO.png"}\'', verbose_name=b'Command line script run by webserver.  Arguments will be passed in JSON format.')),
+                ('script_args', models.TextField(help_text=b'populated on save', blank=True)),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
+        migrations.AddField(
+            model_name='predictdataset',
+            name='status',
+            field=models.ForeignKey(to='predict.PredictDatasetStatus'),
+        ),
+        migrations.AddField(
+            model_name='predictdataset',
+            name='user',
+            field=models.ForeignKey(to='tb_users.TBUser'),
+        ),
+        migrations.AddField(
+            model_name='datasetscriptrun',
+            name='dataset',
+            field=models.ForeignKey(to='predict.PredictDataset'),
+        ),
+    ]
