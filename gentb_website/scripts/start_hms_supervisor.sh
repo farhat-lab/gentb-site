9465c72f56a39ffa27921800ed9fa164eb0b3fe7
Raman Prasad <raman_prasad@harvard.edu>
2015-11-19 14:12:26 -0500

    update

--- gentb_website/scripts/start_hms_supervisor.sh
+++ gentb_website/scripts/start_hms_supervisor.sh
@@ -5,6 +5,6 @@ cd /www/gentb.hms.harvard.edu/code/gentb-site/gentb_website
 source venv_tb/bin/activate
 source venv_tb/bin/postactivate
 
-# Start supervisor
+# Start supervisor (not happening yet....)
 cd /www/gentb.hms.harvard.edu/code/gentb-site/gentb_website/tb_website
 /www/gentb.hms.harvard.edu/code/gentb-site/gentb_website/venv_tb/bin/gunicorn -c tb_website/settings/hms_gunicorn_config.py tb_website.wsgi:application
