1e9e5a100dc560df8585db080e65e3aea850b406
Raman Prasad <raman_prasad@harvard.edu>
2015-11-19 13:25:26 -0500

    start gunicorn cmd

---
+++ gentb_website/scripts/start_gunicorn_cmd.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /www/gentb.hms.harvard.edu/code/gentb-site/gentb_website/tb_website
+/www/gentb.hms.harvard.edu/code/gentb-site/gentb_website/venv_tb/bin/gunicorn -c tb_website/settings/hms_gunicorn_config.py tb_website.wsgi:application
