afc5a258c10d1fc1cd0c093b841aed7d30bfcc0c
Raman Prasad <raman_prasad@harvard.edu>
2015-11-19 13:38:24 -0500

    update path

--- gentb_website/tb_website/tb_website/settings/hms_gunicorn_config.py
+++ gentb_website/tb_website/tb_website/settings/hms_gunicorn_config.py
@@ -1,5 +1,6 @@
-import sys
-sys.path.append('/www/gentb.hms.harvard.edu/code/gentb-site/gentb_website')
+import os, sys
+os.chdir('/www/gentb.hms.harvard.edu/code/gentb-site/gentb_website/tb_website')
+#sys.path.append('/www/gentb.hms.harvard.edu/code/gentb-site/gentb_website')
 
 bind = '0.0.0.0:9001'
 pidfile=None #'gunicorn_pid'
