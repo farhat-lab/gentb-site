0aa92becf3c193b1691b68dbe52f2dbd44d890d6
Raman Prasad <raman_prasad@harvard.edu>
2015-11-19 14:44:55 -0500

    crontab to run supervisor on reboot

--- gentb_website/scripts/start_hms_supervisor.sh
+++ gentb_website/scripts/start_hms_supervisor.sh
@@ -7,13 +7,12 @@ source venv_tb/bin/postactivate
 
 cd /www/gentb.hms.harvard.edu/code/gentb-site/gentb_website/tb_website
 
-# Start supervisor (not happening yet....)
+# Start supervisor
 supervisord -c /www/gentb.hms.harvard.edu/code/gentb-site/gentb_website/tb_website/tb_website/settings/hms_supervisord.conf
 
 # Stop supervisor
-# supervisorctl -c /www/gentb.hms.harvard.edu/code/gentb-site/gentb_website/tb_website/tb_website/settings/hms_supervisord.conf
+# supervisorctl -c /www/gentb.hms.harvard.edu/code/gentb-site/gentb_website/tb_website/tb_website/settings/hms_supervisord.conf shutdown
 
 # For crontab
 #
 #@reboot /www/gentb.hms.harvard.edu/code/gentb-site/gentb_website/scripts/start_hms_supervisor.sh
- 
