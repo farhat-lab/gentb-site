58274a23bb03f26eae4f5d3a52959e463fc6660b
Martin Owens <doctormo@gmail.com>
2016-03-21 19:03:57 -0400

    Add and update gunicorn scripts

---
+++ scripts/gunicorn_config.py
@@ -0,0 +1,32 @@
+import os, sys
+
+from os.path import normpath, dirname, join
+
+# Make sure we always know where we are when running
+ROOT = dirname(normpath(join(os.getenv('PWD'), __file__)))
+
+DATA_DIR = join(ROOT, 'data')
+LOG_DIR = join(DATA_DIR, 'logs', 'gunicorn')
+
+if not os.path.isdir(LOG_DIR):
+    os.makedirs(LOG_DIR)
+
+# Add the root to the python path (so we can find mondules)
+sys.path.append(ROOT)
+
+bind = '0.0.0.0:9002'
+pidfile = join(DATA_DIR, 'gunicorn.pid')
+
+keepalive = 2
+timeout = 30
+workers = 1
+#worker_class = 'sync'
+#worker_connections = 1000
+
+loglevel = 'info'
+
+accesslog = join(LOG_DIR, 'access.log')
+errorlog = join(LOG_DIR, 'error.log')
+#errorlog = '-' # i.e. stdout
+
+proc_name = 'gunicorn_gentb'
--- scripts/start_gunicorn.sh
+++ scripts/start_gunicorn.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# Set up the virtualenv
-cd /www/gentb.hms.harvard.edu/code/gentb-site/gentb_website
-source venv_tb/bin/activate
-source venv_tb/bin/postactivate
+# Go to this directory (where the script is held)
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+cd ..
 
-# Go to genTB Django dir
-cd /www/gentb.hms.harvard.edu/code/gentb-site/gentb_website/tb_website
+# Set up the virtualenv
+source pythonenv/bin/activate
 
 # Start gunicorn
-/www/gentb.hms.harvard.edu/code/gentb-site/gentb_website/venv_tb/bin/gunicorn -c tb_website/settings/hms_gunicorn_config.py tb_website.wsgi:application
+pythonenv/bin/gunicorn -c scripts/gunicorn_config.py tb_website.wsgi:application
  
