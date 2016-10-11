bffce884243ef3eb935972a48b311768513f9de3
Martin Owens <doctormo@gmail.com>
2016-10-11 01:40:35 -0400

    Have refresh deleted before restart

--- scripts/watchdog.sh
+++ scripts/watchdog.sh
@@ -3,6 +3,6 @@
 # Watch for a refresh file and restart server.
 #
 if [[ -f refresh_me ]]; then
-  ./scripts/supervisorctl.sh restart all
   rm refresh_me
+  ./scripts/supervisorctl.sh restart all
 fi
