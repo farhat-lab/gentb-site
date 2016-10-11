c3a9978a355d962ecb0026d84e566cab4196335b
Martin Owens <doctormo@gmail.com>
2016-10-10 23:25:44 -0400

    Implement datalist for manual input, improve css too

--- apps/mutations/static/js/lister.js
+++ apps/mutations/static/js/lister.js
@@ -9,8 +9,9 @@
   $.fn.replaceOptions = function(options) {
     this.empty();
     var self = this;
-    $(self).addOption("---", '---');
-
+    if($(self).is('select')) {
+      $(self).addOption("---", '---');
+    }
     $.each(options, function(index, option) {
       var opt = $(self).addOption(option.name, option.value);
       opt.data('children', option.children);
@@ -26,41 +27,77 @@ $(document).ready(function() {
     $.getJSON($(this).data('data-url'), function(data) {
       target.data('data', data);
       $.each(data.levels, function(index, level) {
-        var select = $('<select title="'+level+'"></select>');
-        if(last_id != null) {
-          $('#' + last_id).data('next', select);
-          select.data('previous', $('#' + last_id));
+        if(index == 2) {
+            var input = $('<input type="text"></input>');
+            input.attr('id', "level-"+index);
+            input.attr('list', "list-"+index);
+            input.attr('title', level);
+            input.data('next', 'button');
+            input.attr('style', 'width: calc(100% - 45px);');
+            input.insertBefore(target);
+
+            var select = $('<datalist></datalist>');
+            select.attr('id', "list-"+index);
+            select.insertBefore(target);
+
+            input.select(function() {
+              var value = $(this).val();
+              if(value && value != '---') {
+                $('#level-button').removeClass('disabled');
+              } else {
+                $('#level-button').addClass('disabled');
+              }
+            });
+        } else {
+            var select = $('<select></select>');
+            select.data('previous', index - 1);
+            select.data('next', index + 1);
+            select.attr('title', level);
+            select.attr('style', 'width: 50%;');
+            select.attr('id', 'level-' + index);
+            select.insertBefore(target);
+            select.replaceOptions([]);
+
+            select.change(function() {
+              var selected = this.selectedOptions;
+              var next_id = $(this).data('next');
+              var next = $('#level-' + next_id);
+              if(!selected || selected[0].label == '---') {
+                next.prop("disabled", true);
+                if(next.is('select')) {
+                  next.val('---');
+                } else {
+                  next.val('');
+                }
+              } else {
+                next.prop("disabled", false);
+                var children = $(selected[0]).data('children');
+                var list = $('#list-' + next_id);
+                if(children) {
+                  if(list.length == 1) {
+                      list.replaceOptions(children);
+                  } else {
+                      next.replaceOptions(children);
+                  }
+                }
+              }
+              next.change();
+              next.select();
+            });
         }
-        last_id = 'level-'+index;
-        select.attr('id', last_id);
-        select.insertBefore(target);
-        select.replaceOptions([]);
-        select.change(function() {
-          var selected = this.selectedOptions;
-          if(!selected || selected[0].label == '---') {
-            $(this).data('next').hide();
-            $(this).data('next').val('---');
-          } else {
-            $(this).data('next').show();
-            var children = $(selected[0]).data('children');
-            if(children) {
-              $(this).data('next').replaceOptions(children);
-            }
-          }
-          $(this).data('next').change();
-        })
       });
 
-      var add_button = $('<a class="btn btn-success">Add</button>');
+      var add_button = $('<a class="btn btn-success btn-sm">Add</button>');
       add_button.insertBefore(target);
+      add_button.attr('id', 'level-button');
+      add_button.attr('style', 'width: 45;');
       add_button.click(function() {
-        var value = $('#'+last_id).val();
+        var value = $('#level-2').val();
         if(value && value != '---') {
           $(target).val(value + '\n' + $(target).val());
+          $('#level-2').val('').select();
         }
       });
-      $('#' + last_id).data('next', add_button);
-
       $('#level-0').replaceOptions(data.children);
       $('#level-0').show();
       $('#level-0').change();
