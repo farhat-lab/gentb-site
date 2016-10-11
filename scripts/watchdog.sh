9ddad0b94f708d1f2c64e55ff1c49432ec5f436a
Martin Owens <doctormo@gmail.com>
2016-10-11 01:46:23 -0400

    Fix: make refresh file consistant

--- scripts/watchdog.sh
+++ scripts/watchdog.sh
@@ -2,7 +2,7 @@
 #
 # Watch for a refresh file and restart server.
 #
-if [[ -f refresh_me ]]; then
-  rm refresh_me
+if [[ -f restart_me ]]; then
+  rm restart_me
   ./scripts/supervisorctl.sh restart all
 fi
