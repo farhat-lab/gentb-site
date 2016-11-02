3d729513a29940e2c187a6e8f1882d323d60a3fb
Martin Owens <doctormo@gmail.com>
2016-11-02 14:41:05 -0400

    Rip out terrible custom user form code

--- apps/tb_users/forms.py
+++ apps/tb_users/forms.py
@@ -3,13 +3,14 @@ import re
 
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from apps.tb_users.models import TBUser
 
 MINIMUM_PASSWORD_LENGTH = 7
 
-class SignUpForm(forms.ModelForm):
+
+class SignUpForm(UserCreationForm):
 
     error_messages = {
         'password_mismatch': _("The two password fields didn't match."),
@@ -18,62 +19,13 @@ class SignUpForm(forms.ModelForm):
     }
 
     affiliation = forms.CharField(label='Affiliation', max_length=255)
-    retype_password = forms.CharField(label='Retype Password', widget=forms.PasswordInput())
 
-    def clean_email(self):
-        email = self.cleaned_data.get('email')
-        username = self.cleaned_data.get('username')
-        if email and User.objects.filter(email=email).exclude(username=username).count():
-            raise forms.ValidationError(u'Email addresses must be unique.')
-        return email
+    def save(self, **kw):
+        # save User, made active
+        user = super(SignUpForm, self).save(commit=False)
+        user.is_active = True
+        user.save()
 
-    def clean_password(self):
-        password = self.cleaned_data.get('password')
-
-        if len(password) < MINIMUM_PASSWORD_LENGTH:
-            raise forms.ValidationError(
-                self.error_messages['password_complexity_length'],
-                code='password_complexity_length',
-        )
-
-        #check if contains a digit
-        if not re.search(r'\d', password):
-            raise forms.ValidationError(
-                self.error_messages['password_complexity_content'],
-                code='password_complexity_content',
-            )
-
-        # check for at least 1 letter
-        if not (re.search(r'[A-Z]', password) or re.search(r'[a-z]', password)):
-            raise forms.ValidationError(
-                self.error_messages['password_complexity_content'],
-                code='password_complexity_content',
-            )
-
-        return password
-
-    def clean_retype_password(self):
-        password = self.cleaned_data.get('password')
-        retype_password = self.cleaned_data.get('retype_password')
-        if password and retype_password:
-            if password != retype_password:
-                raise forms.ValidationError(
-                    self.error_messages['password_mismatch'],
-                    code='password_mismatch',
-                )
-        return retype_password
-
-    def create_tb_user(self):        
-        assert hasattr(self, 'cleaned_data'), "Do not call this method on an invalid form."
-
-        # save User, made inactive
-        #
-        user = self.save(commit=False)  # get object
-        #user.is_active = False  # set to inactive, use for activation link scenario
-        user.save() # save
-
-        # save TBUser
-        #
         tb_user = TBUser(user=user, affiliation=self.cleaned_data['affiliation'])
         tb_user.save()
 
@@ -81,16 +33,7 @@ class SignUpForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ['username', 'password', 'retype_password', 'first_name', 'last_name', 'email', 'affiliation']
-
-    def __init__(self, *args, **kwargs):
-        super(SignUpForm, self).__init__(*args, **kwargs)
-
-        for key in self.fields:
-            self.fields[key].required = True
-
-        self.fields['password'].widget = forms.PasswordInput()
-        #password = forms.CharField(widget=forms.PasswordInput())
+        fields = ['username', 'password1', 'password2', 'first_name', 'last_name', 'email', 'affiliation']
 
 
 class LoginForm(AuthenticationForm):
