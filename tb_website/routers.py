cb8a1291e51b4c29e6c6959325914d086e6dcc53
Martin Owens <doctormo@gmail.com>
2016-11-09 12:50:14 -0500

    Add private app routers

---
+++ tb_website/routers.py
@@ -0,0 +1,22 @@
+
+from django.conf import settings
+DB = list(settings.DATABASES)
+
+class PrivateAppRouter(object):
+    """
+    Allows an app to have it's own private database if needed
+    """
+    db_for_write = db_for_read
+
+    def db_for_read(self, model, **hints):
+        if model._meta.app_label in DB:
+            return model._meta.app_label
+
+    def allow_relation(self, obj1, obj2, **hints):
+        if obj1._state.db == obj2._state.db:
+            return True
+        return None
+
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        return app_label not in DB
+
