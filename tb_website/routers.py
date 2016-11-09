d68557f52b69a1a51775ff7b2b61e8b649986152
Martin Owens <doctormo@gmail.com>
2016-11-09 12:59:11 -0500

    Fix simple mistake

--- tb_website/routers.py
+++ tb_website/routers.py
@@ -6,11 +6,10 @@ class PrivateAppRouter(object):
     """
     Allows an app to have it's own private database if needed
     """
-    db_for_write = db_for_read
-
     def db_for_read(self, model, **hints):
         if model._meta.app_label in DB:
             return model._meta.app_label
+    db_for_write = db_for_read
 
     def allow_relation(self, obj1, obj2, **hints):
         if obj1._state.db == obj2._state.db:
