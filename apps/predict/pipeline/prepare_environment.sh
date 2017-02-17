657929ce2554f57c2520a91421d452e5c370eff6
Martin Owens <doctormo@gmail.com>
2017-02-17 11:26:04 -0500

    Remove all old pipeline code and data files

--- apps/predict/pipeline/prepare_environment.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#load all necessary modules
-module load dev/perl/5.18.1
-module load dev/python/2.7.10
-module load seq/samtools/1.2
-module load seq/stampy/1.0.23
-module load seq/Platypus/0.8.1
-module load stats/R/2.15.3
