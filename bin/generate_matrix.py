6f09f883db7f1c34ed23e8ee6373d4f685cd9b8e
Martin Owens <doctormo@gmail.com>
2017-03-02 11:34:07 -0500

    Update generate_matrix so it outputs to stdout a single strain at a time

--- bin/generate_matrix.py
+++ bin/generate_matrix.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 #code that generates the matrix.csv from the .var files
 
 import sys
@@ -7,31 +8,27 @@ import os
 import re
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
-#SCRIPT_DIR= sys.argv[2]
 
 variants=set()
-for line in open(SCRIPT_DIR + "/variant_name_list.csv"):
+for line in open(sys.argv[1]):
     variants=line.rstrip().split(",")
 
 d = defaultdict(set)
-strains=set()
-for filename in os.listdir(sys.argv[1]):
-    if filename.endswith(".var"):
-        sn=filename.split(".")[0] #strain name
-        filen=sys.argv[1]+"/"+filename
-        #filen='/home/maha/gentb/gentb-site/apps/predict/pipeline/bin'+"/"+filename
-        for line in open(filen):
-            e=line.rstrip().split("\t")
-            d[sn].add(e[3]) #keys are strainnames and values are variantnames ie. snpnames
-            strains.add(sn)
-            #print >>sys.stderr, d[e[3]]
+strains = set()
+filename = sys.argv[2]
+if filename.endswith(".var"):
+    sn = filename.rsplit(".", 1)[0] #strain name
+    for line in open(filename):
+        e=line.rstrip().split("\t")
+        d[sn].add(e[3]) #keys are strainnames and values are variantnames ie. snpnames
+        strains.add(sn)
+        #print >>sys.stderr, d[e[3]]
 
 variants=list(set(variants))
 strains=list(strains)
 #print >>sys.stderr, sys.argv[1]
 
-f=sys.argv[1]+"/matrix.csv"
-output2=file(f,'w')
+output2=sys.stdout
 output2.write('strain,')
 output2.write(",".join(variants))
 output2.write("\n")
