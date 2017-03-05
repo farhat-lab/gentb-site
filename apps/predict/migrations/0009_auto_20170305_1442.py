a55d7e0fb300faa72822effb62bedbac46c4b1be
Martin Owens <doctormo@gmail.com>
2017-03-05 14:42:52 -0500

    Add migration for predict foreignkey dependance

---
+++ apps/predict/migrations/0009_auto_20170305_1442.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('predict', '0008_auto_20170214_0945'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='predictstrain',
+            name='file_one',
+            field=models.ForeignKey(related_name='link_a', on_delete=django.db.models.deletion.SET_NULL, blank=True, to='dropbox.DropboxFile', null=True),
+        ),
+        migrations.AlterField(
+            model_name='predictstrain',
+            name='file_two',
+            field=models.ForeignKey(related_name='link_b', on_delete=django.db.models.deletion.SET_NULL, blank=True, to='dropbox.DropboxFile', null=True),
+        ),
+        migrations.AlterField(
+            model_name='predictstrain',
+            name='piperun',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, blank=True, to='pipeline.PipelineRun', null=True),
+        ),
+    ]
