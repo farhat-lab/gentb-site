f757d21ce93d4e1b6cbe9cc4629ce245260701bf
Martin Owens <doctormo@gmail.com>
2017-03-10 12:58:15 -0500

    git indication we're running in privilaged database

--- tb_website/settings/manage.py
+++ tb_website/settings/manage.py
@@ -3,8 +3,11 @@ MANAGER_DB = None
 
 from .local import *
 
+print "Manager Django Backend!"
+
 if MANAGER_DB is not None:
     for db in DATABASES:
         if db in MANAGER_DB:
+            print "Updating database with MANAGER credentials."
             DATABASES[db].update(MANAGER_DB[db])
 
