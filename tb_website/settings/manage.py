2a27b7db622f2b607a9a1a66afa16a40729cd3d8
Martin Owens <doctormo@gmail.com>
2017-03-16 19:15:30 -0400

    Always go to stderr instead of stdout on management commands

--- tb_website/settings/manage.py
+++ tb_website/settings/manage.py
@@ -2,12 +2,13 @@
 MANAGER_DB = None
 
 from .local import *
+import sys
 
-print "Manager Django Backend!"
+sys.stderr.write("Manager Django Backend!\n")
 
 if MANAGER_DB is not None:
     for db in DATABASES:
         if db in MANAGER_DB:
-            print "Updating database with MANAGER credentials."
+            sys.stderr.write("Updating database with MANAGER credentials.\n")
             DATABASES[db].update(MANAGER_DB[db])
 
