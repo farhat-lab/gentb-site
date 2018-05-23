90f9ca68e507de438ec104948d9a436cf409b500
Martin Owens <doctormo@gmail.com>
2018-05-23 13:16:36 -0400

    Set remove pipeline runs on fatal errors

--- apps/predict/management/commands/run_pipeline.py
+++ apps/predict/management/commands/run_pipeline.py
@@ -44,6 +44,9 @@ class Command(BaseCommand):
 
         for progrun in ProgramRun.objects.filter(is_submitted=True, is_complete=False, is_error=False):
             sys.stderr.write("Run Status: %s " % str(progrun))
+            if 'fatal' in str(progrun).lower():
+                progrun.is_error = True
+                progrun.save()
             if progrun.update_status():
                 sys.stderr.write(" [COMPLETE]\n")
                 progrun.piperun.predictstrain_set.first().update_status()
