ccb021e4f672b02d63236207573cc5f7746012e2
Martin Owens <doctormo@gmail.com>
2018-05-24 21:47:52 -0400

    Mark resuable uploads as broken if they are

--- apps/uploads/management/commands/process_uploads.py
+++ apps/uploads/management/commands/process_uploads.py
@@ -1,21 +1,31 @@
-
+"""Download from urls any uploads from outside sources"""
 import logging
+
+from django.utils.timezone import now
+from django.core.management.base import BaseCommand, CommandError
+from apps.uploads.models import DropboxUploadFile, ManualUploadFile, ResumableUploadFile
+
 LOGGER = logging.getLogger('apps.uploads')
 
-from django.core.management.base import BaseCommand, CommandError
-from apps.uploads.models import DropboxUploadFile, ManualUploadFile
-
 class Command(BaseCommand):
-    help = """Regular run of new dropbox links:
+    """Run command for uploads"""
+    help = __doc__ + """:
 
       manage.py process_uploads
 
     """
-    def handle(self, **options):
-        for cls in [DropboxUploadFile, ManualUploadFile]:
+    @staticmethod
+    def handle(**_):
+        """Handle script call"""
+        for cls in (DropboxUploadFile, ManualUploadFile):
             for d_file in cls.objects.filter(retrieval_start__isnull=True):
                 print " + Downloading: %s" % d_file.url
                 d_file.download_now()
                 if d_file.retrieval_error:
                     print " ! Error downloading"
-
+        count = ResumableUploadFile.objects.filter(retrieval_start__isnull=True).update(
+            retrieval_error='Retry resumable upload, can not happen in server.',
+            retrieval_start=now(),
+        )
+        if count:
+            print(" * Resumable uploads marked as impossible: {}".format(count))
