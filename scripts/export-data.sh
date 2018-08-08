fe0a99102aade4681890beedcfaeda5579aa6c97
Martin Owens <doctormo@gmail.com>
2018-08-08 10:22:41 -0400

    Delete old production scripts

--- scripts/export-data.sh
+++
@@ -1,3 +0,0 @@
-
-./manage dumpdata --indent=2 -e auth.permission -e contenttypes -e sessions -e explore -e admin -o $1 
-
--- scripts/watchdog.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# Watch for a refresh file and restart server.
-#
-if [[ -f restart_me ]]; then
-  rm restart_me
-  ./scripts/supervisorctl.sh restart gentb_website
-fi
