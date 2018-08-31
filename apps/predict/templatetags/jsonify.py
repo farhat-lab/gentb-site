3faa574c07757db3ee7c41fa96c3de534edc5ca9
Martin Owens <doctormo@gmail.com>
2018-08-31 16:12:55 -0400

    Remove single quote marks

--- apps/predict/templatetags/jsonify.py
+++ apps/predict/templatetags/jsonify.py
@@ -12,4 +12,4 @@ def jsonify(obj):
     """Turn object into a json instance"""
     if isinstance(obj, types.GeneratorType):
         obj = list(obj)
-    return mark_safe(json.dumps(obj).replace("'", "\\'"))
+    return mark_safe(json.dumps(obj).replace("'", ""))
