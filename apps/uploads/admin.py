8ba57727aacf29ec7903de6b599adc7aed609943
Martin Owens <doctormo@gmail.com>
2018-10-05 10:50:15 -0400

    Improve admin interface for uploaded files

--- apps/uploads/admin.py
+++ apps/uploads/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import UploadFile
 
 class UploadFileAdmin(admin.ModelAdmin):
+    list_display = ('title', 'get_type', 'flag', 'size', 'created', 'retrieval_start', 'retrieval_end', 'retrieval_error')
     save_on_top = True
+
+    def title(self, obj):
+        """Returns the uploads name plus icon (if available)"""
+        if obj.icon:
+            return mark_safe("<img src='{0.icon}' style='width: 2em; vertical-align: middle;'> {0.name}".format(obj))
+        return obj.name
 
 admin.site.register(UploadFile, UploadFileAdmin)
