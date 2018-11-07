404c515adce7128ca6841dcb833fb605ec0950a2
Martin Owens <doctormo@gmail.com>
2018-11-07 13:10:17 -0500

    Add natural keys for map models

--- apps/maps/gis.py
+++ apps/maps/gis.py
@@ -4,7 +4,7 @@ Provide a little bit of abstraction for gis.
 from django.db.models import TextField, Manager
 
 try:
-    from django.contrib.gis.db.models import *
+    from django.contrib.gis.db.models import GeoManager, MultiPolygonField, MultiPointField
 except Exception:
     def MultiPolygonField(*args, **kw):
         return TextField()
--- apps/maps/models.py
+++ apps/maps/models.py
@@ -3,54 +3,60 @@ Provide a place for city and country geo data for showing on maps.
 """
 import os
 
-from django.db.models import *
+from django.db.models import (
+    Model, OneToOneField, ForeignKey, IntegerField, FloatField, CharField,
+)
 from django.utils.encoding import python_2_unicode_compatible
 
-from .gis import *
+from .gis import GeoManager, MultiPolygonField, MultiPointField
 
 # This is where we are currently looking for data, but it could change.
-url_prefix = 'http://www.naturalearthdata.com/'\
+URL_PREFIX = 'http://www.naturalearthdata.com/'\
     'http//www.naturalearthdata.com/download/10m/cultural/'
 
 # UN Regions and sub-regions
 REGIONS = (
-  (0, 'Other'),
-  (1, 'World'),
-  (2, 'Agrica'),
-  (9, 'Oceania'),
-  (19, 'Americas'),
-  (21, 'North America'),
-  (142, 'Asia'),
-  (150, 'Europe'),
-  (419, 'Latin America and the Caribbean'),
+    (0, 'Other'),
+    (1, 'World'),
+    (2, 'Agrica'),
+    (9, 'Oceania'),
+    (19, 'Americas'),
+    (21, 'North America'),
+    (142, 'Asia'),
+    (150, 'Europe'),
+    (419, 'Latin America and the Caribbean'),
 )
 SUB_REGIONS = (
-  (14, 'Eastern Africa'),
-  (17, 'Middle Africa'),
-  (15, 'Northern Africa'),
-  (18, 'Southern Africa'),
-  (11, 'Western Africa'),
+    (14, 'Eastern Africa'),
+    (17, 'Middle Africa'),
+    (15, 'Northern Africa'),
+    (18, 'Southern Africa'),
+    (11, 'Western Africa'),
 
-  (29, 'Caribbean'),
-  (13, 'Central America'),
-  (5, 'South America'),
+    (29, 'Caribbean'),
+    (13, 'Central America'),
+    (5, 'South America'),
 
-  (143, 'Central Asia'),
-  (30, 'Eastern Asia'),
-  (34, 'Southern Asia'),
-  (35, 'South-Eastern Asia'),
-  (145, 'Western Asia'),
+    (143, 'Central Asia'),
+    (30, 'Eastern Asia'),
+    (34, 'Southern Asia'),
+    (35, 'South-Eastern Asia'),
+    (145, 'Western Asia'),
 
-  (151, 'Eastern Europe'),
-  (154, 'Northern Europe'),
-  (39, 'Southern Europe'),
-  (155, 'Western Europe'),
+    (151, 'Eastern Europe'),
+    (154, 'Northern Europe'),
+    (39, 'Southern Europe'),
+    (155, 'Western Europe'),
 
-  (53, 'Australia and New Zealand'),
-  (54, 'Melanesia'),
-  (57, 'Micronesia'),
-  (61, 'Polynesia'),
+    (53, 'Australia and New Zealand'),
+    (54, 'Melanesia'),
+    (57, 'Micronesia'),
+    (61, 'Polynesia'),
 )
+
+class CountryManager(GeoManager):
+    def get_by_natural_key(self, cid):
+        return self.get(iso2=cid)
 
 @python_2_unicode_compatible
 class Country(Model):
@@ -64,11 +70,11 @@ class Country(Model):
     subregion = IntegerField(choices=SUB_REGIONS, null=True, blank=True)
 
     geom = MultiPolygonField(srid=4326)
-    objects = GeoManager()
+    objects = CountryManager()
 
     online_zip = 'http://thematicmapping.org/downloads/TM_WORLD_BORDERS_SIMPL-0.3.zip'
     mapping_id = 'iso2'
-    mapping = { 
+    mapping = {
         'iso2': 'ISO2',
         'iso3': 'ISO3',
         'geom': 'POLYGON',
@@ -81,13 +87,17 @@ class Country(Model):
         verbose_name_plural = 'countries'
         ordering = ('name',)
 
+    def natural_key(self):
+        """We want the drug code to be the key into this table"""
+        return (self.iso2,)
+
     def __str__(self):
         return self.name
 
 
 @python_2_unicode_compatible
 class CountryDetail(Model):
-    """Provides a much more details country shape and more fields"""
+    """Provides a much more detailed country outline and more fields"""
     country = OneToOneField(Country, related_name='detail')
 
     name_short = CharField(max_length=36, null=True, blank=True)
@@ -102,7 +112,7 @@ class CountryDetail(Model):
 
     geom = MultiPolygonField(srid=4326)
 
-    online_zip = os.path.join(url_prefix, 'ne_10m_admin_0_countries.zip')
+    online_zip = os.path.join(URL_PREFIX, 'ne_10m_admin_0_countries.zip')
     mapping_id = 'country'
     mapping = {
         'country' : {'iso2': 'ISO_A2'},
@@ -114,13 +124,17 @@ class CountryDetail(Model):
         'continent' : 'CONTINENT',
         'geom' : 'MULTIPOLYGON',
     }
-    
+
     class Meta:
         ordering = ('-pop',)
 
     def __str__(self):
         return self.name_short or self.name_abbr or self.country_id
 
+
+class PlaceManager(GeoManager):
+    def get_by_natural_key(self, name, country):
+        return self.get(name=name, country=country)
 
 @python_2_unicode_compatible
 class Place(Model):
@@ -137,11 +151,11 @@ class Place(Model):
     elevation = FloatField()
     timezone = CharField(max_length=254)
     geom = MultiPointField(srid=4326)
-    objects = GeoManager()
+    objects = PlaceManager()
 
-    online_zip = os.path.join(url_prefix, 'ne_10m_populated_places.zip')
+    online_zip = os.path.join(URL_PREFIX, 'ne_10m_populated_places.zip')
     mapping_id = ('name', 'country')
-    mapping = { 
+    mapping = {
         'pop' : 'GN_POP',
         'rank' : 'SCALERANK',
         'name' : 'NAME',
@@ -151,11 +165,15 @@ class Place(Model):
         'elevation' : 'ELEVATION',
         'timezone' : 'TIMEZONE',
         'geom' : 'MULTIPOINT',
-    }   
+    }
 
     class Meta:
         ordering = ('-pop',)
+        unique_together = ('name', 'country',)
+
+    def natural_key(self):
+        """Return a natural key for places"""
+        return (self.name, self.country.iso2)
 
     def __str__(self):
         return self.name
-
