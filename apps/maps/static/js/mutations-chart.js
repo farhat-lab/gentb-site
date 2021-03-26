01141e0288c7b13fed0fde342f91dc41536828f1
Martin Owens <doctormo@gmail.com>
2021-03-25 20:23:57 -0400

    Revert "Stop sorting the strain count"
    This reverts commit 8fbec33da57a43f35e6098e2d5d81f4e3534d376.

--- apps/maps/static/js/mutations-chart.js
+++ apps/maps/static/js/mutations-chart.js
@@ -149,7 +149,7 @@ $(document).ready(function() {
 	    { "data": "aminoacid_reference", "title": "aaref" },
 	    { "data": "aminoacid_position", "title": "aapos" },
             { "data": "aminoacid_varient", "title": "aavar" },
-            { "data": "strain_count", "title": "Strains", "orderable": false },
+            { "data": "strain_count", "title": "Strains" },
           ],
           'order': [[2, 'asc']],
         })
