bcd8ace9f4b4cb84becf613f16a6b0125baddf1d
Martin Owens <doctormo@gmail.com>
2021-04-19 06:23:00 -0400

    Reform to focus on mutation objects directly

--- apps/mutations/management/commands/count_strains.py
+++ apps/mutations/management/commands/count_strains.py
@@ -8,14 +8,46 @@ import sys
 import logging
 
 from collections import defaultdict
+
+from django.db import transaction
 from django.core.management.base import BaseCommand, CommandError
 
-from apps.mutations.models import StrainMutationCache, StrainMutation
+from apps.mutations.models import StrainMutationCache, StrainMutation, Mutation
 
 class Command(BaseCommand):
     __doc__
+    def __init__(self):
+        self.rejected = 0
+        self.rejects = defaultdict(int)
+
     def handle(self, **_):
         """Handle the command being called"""
+        # Clear caching count database
+        count, details = StrainMutationCache.objects.all().delete()
+        print(f" [x] Deleted {count} existing caching rows.")
+
+        total_cache = 0
+        total_mut = Mutation.objects.count()
+        filters = list(StrainMutationCache.matrix_filter())
+        filter_names = list(StrainMutationCache.filters)
+        print(f" [x] Ready to count {total_mut} Mutations.")
+
+        for mut_x, mutation in enumerate(Mutation.objects.order_by('pk')):
+            # Count all the StrainMutations
+            if mut_x % 100 == 0:
+                pc = mut_x / total_mut * 100
+                sys.stdout.write(f" [/] Mutation: {mut_x} ({pc:0.2g}%) {total_cache} counts, {self.rejected} rejected\r")
+                sys.stdout.flush()
+
+            total_cache += self._count(mutation, filters, filter_names)
+
+        sys.stdout.write(f" [x] Mutation: {mut_x} (100%) {total_cache} counts, {self.rejected} rejected\r")
+        sys.stdout.flush()
+        print("\n")
+
+    @transaction.atomic
+    def _count(self, mutation, filters, filter_names):
+        """Count up all the strains linked to this mutation"""
         def _getattr(obj, name, enabled):
             if enabled:
                 return None
@@ -24,65 +56,41 @@ class Command(BaseCommand):
                 return False
             return ret
 
-        # Clear caching count database
-        count, details = StrainMutationCache.objects.all().delete()
-        print(f" [x] Deleted {count} existing caching rows.")
-
-        total = StrainMutation.objects.count()
-        filters = list(StrainMutationCache.matrix_filter())
-        filter_names = list(StrainMutationCache.filters)
-        print(f" [x] Ready to count {total} StrainMutations.")
-
         counts = defaultdict(int)
-        rejected = 0
-        rejects = defaultdict(int)
 
-        # Count all the StrainMutations
-        for y in range(0, total, 100):
-            pc = (y) / total * 100
-            sys.stdout.write(f" [/] StrainMutation: {y} ({pc:0.2g}%)   \r")
-            sys.stdout.flush()
-            for x, st_mut in enumerate(StrainMutation.objects.order_by('pk')[y:y+100]):
-                if x % 50 == 5:
-                    pc = (y + x) / total * 100
-                    ct = len(counts)
-                    rej = ', '.join([filter_names[r_i] + f': {r_j}' for r_i, r_j in rejects.items()])
-                    sys.stdout.write(f" [\] StrainMutation: {y}+{x} ({pc:0.2g}%) {ct} counts, {rejected} rejected {rej}      \r")
-                    sys.stdout.flush()
-                # for each combination of the given fields
-                for combo in filters:
-                    # add one to this CacheCount
-                    key = tuple([_getattr(st_mut.strain, name, enabled) for name, enabled in combo])
-                    # Reject keys where the required field wasn't even set. This combinatin is invalid
-                    if False in key:
-                        rejected += 1
-                        for x, k in enumerate(key):
-                            if k is False:
-                                rejects[x] += 1
-                    else:
-                        counts[(st_mut.mutation,) + key] += 1
+        for stmut_x, st_mut in enumerate(mutation.strain_mutations.order_by('pk')):
+            # for each combination of the given fields
+            for combo in filters:
+                # add one to this CacheCount
+                key = tuple([_getattr(st_mut.strain, name, enabled) for name, enabled in combo])
+                # Reject keys where the required field wasn't even set. This combinatin is invalid
+                if False in key:
+                    self.rejected += 1
+                    for x, k in enumerate(key):
+                        if k is False:
+                            self.rejects[x] += 1
+                else:
+                    counts[key] += 1
 
-        print(f" [x] MutationStrains counted: {x} (100%) generated {ct} counts, {rejected} rejected    ")
+        #print(f" [x] MutationStrains counted: {x} (100%) generated {ct} counts, {rejected} rejected    ")
         rejected = 0
-        c = 0
-        x = y + x + 1
         total = len(counts)
-        print(f" [x] Saving {ct} counts...")
-        for x, ((mutation, *fields), count) in enumerate(counts.items()):
-            if x % 100 == 0:
-                c = x - rejected
-                pc = x / total * 100
-                sys.stdout.write(f" [ ] Caches: {c} ({pc:0.2g}%) {c} Saved {rejected} counts too small.\r")
-                sys.stdout.flush()
+        #print(f" [x] Saving {ct} counts...")
+        for x, (fields, count) in enumerate(counts.items()):
+            #if x % 100 == 0:
+                #c = x - rejected
+                #pc = x / total * 100
+                #sys.stdout.write(f" [ ] Caches: {c} ({pc:0.2g}%) {c} Saved {rejected} counts too small.\r")
+                #sys.stdout.flush()
             if count <= 1:
                 # We're not going to count single items, this is sort sorting only.
                 rejected += 1
                 continue
+
             kwargs = dict(zip(filter_names, fields))
             StrainMutationCache.objects.create(
                 mutation=mutation,
                 count=count,
                 **kwargs)
 
-        x += 1
-        sys.stdout.write(f" [x] Caches: {x} (100%), {c} Saved {rejected} counts too small.\n")
+        return total - rejected
