850484445afc40b1a958a18fc30a65ebd8bef9a4
Martin Owens <doctormo@gmail.com>
2021-04-19 04:43:45 -0400

    Space out requests to chunk through the database

--- apps/mutations/management/commands/count_strains.py
+++ apps/mutations/management/commands/count_strains.py
@@ -26,35 +26,41 @@ class Command(BaseCommand):
 
         # Clear caching count database
         count, details = StrainMutationCache.objects.all().delete()
-        print(f"Deleted {count} existing caching rows.")
+        print(f" [x] Deleted {count} existing caching rows.")
 
         total = StrainMutation.objects.count()
         filters = StrainMutationCache.matrix_filter()
         filter_names = list(StrainMutationCache.filters)
-        print(f"Ready to count {total} StrainMutations")
+        print(f" [ ] Ready to count {total} StrainMutations.")
 
         counts = defaultdict(int)
 
         # Count all the StrainMutations
-        for x, st_mut in enumerate(StrainMutation.objects.all()):
-            if x % 200 == 0:
-                pc = x / total * 100
-                ct = len(counts)
-                sys.stdout.write(f"StrainMutation: {x} ({pc}%) generated {ct} counts so far\r")
-                sys.stdout.flush()
-            # for each combination of the given fields
-            for combo in filters:
-                # add one to this CacheCount
-                key = tuple([_getattr(st_mut.strain, name, enabled) for name, enabled in combo])
-                # Reject keys where the required field wasn't even set. This combinatin is invalid
-                if False in key:
-                    continue
-                counts[(st_mut.mutation,) + key] += 1
+        for y in range(0, total, 100):
+            pc = (y) / total * 100
+            sys.stdout.write(f"StrainMutation: {y} ({pc:0.2g}%)\r")
+            sys.stdout.flush()
+            for x, st_mut in enumerate(StrainMutation.objects.order_by('pk')[y:y+100]):
+                if x % 10 == 0:
+                    pc = (y + x) / total * 100
+                    ct = len(counts)
+                    sys.stdout.write(f"StrainMutation: {y}+{x} ({pc:0.2g}%) generated {ct} counts so far    \r")
+                    sys.stdout.flush()
+                # for each combination of the given fields
+                for combo in filters:
+                    # add one to this CacheCount
+                    key = tuple([_getattr(st_mut.strain, name, enabled) for name, enabled in combo])
+                    # Reject keys where the required field wasn't even set. This combinatin is invalid
+                    if False in key:
+                        continue
+                    counts[(st_mut.mutation,) + key] += 1
 
+        x = y + x + 1
         total = len(counts)
-        print("\nSaving counts...\n")
+        print(f" [ ] MutationStrains counted: {x} (100%) generated {ct} counts so far    ")
+        print(f" [ ] Saving {ct} counts...")
         for x, ((mutation, *fields), count) in enumerate(counts.items()):
-            if x % 200 == 0:
+            if x % 100 == 0:
                 pc = x / total * 100
                 sys.stdout.write(f"Caches Saved: {x} ({pc}%)\r")
                 sys.stdout.flush()
@@ -64,3 +70,5 @@ class Command(BaseCommand):
                 count=count,
                 **kwargs)
 
+        x += 1
+        sys.stdout.write(f" [x] Caches Saved: {x} (100%)\n")
