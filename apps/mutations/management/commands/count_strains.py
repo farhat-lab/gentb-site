b5920099e1f5e3a81a2b902c71df29d87c86effe
Martin Owens <doctormo@gmail.com>
2021-04-19 04:48:54 -0400

    Count rejected in strain mutation counter

--- apps/mutations/management/commands/count_strains.py
+++ apps/mutations/management/commands/count_strains.py
@@ -34,17 +34,18 @@ class Command(BaseCommand):
         print(f" [ ] Ready to count {total} StrainMutations.")
 
         counts = defaultdict(int)
+        rejected = 0
 
         # Count all the StrainMutations
         for y in range(0, total, 100):
             pc = (y) / total * 100
-            sys.stdout.write(f"StrainMutation: {y} ({pc:0.2g}%)\r")
+            sys.stdout.write(f"StrainMutation: {y} ({pc:0.2g}%)   \r")
             sys.stdout.flush()
             for x, st_mut in enumerate(StrainMutation.objects.order_by('pk')[y:y+100]):
                 if x % 10 == 0:
                     pc = (y + x) / total * 100
                     ct = len(counts)
-                    sys.stdout.write(f"StrainMutation: {y}+{x} ({pc:0.2g}%) generated {ct} counts so far    \r")
+                    sys.stdout.write(f"StrainMutation: {y}+{x} ({pc:0.2g}%) {ct} counts, {rejected} rejected       \r")
                     sys.stdout.flush()
                 # for each combination of the given fields
                 for combo in filters:
@@ -52,6 +53,7 @@ class Command(BaseCommand):
                     key = tuple([_getattr(st_mut.strain, name, enabled) for name, enabled in combo])
                     # Reject keys where the required field wasn't even set. This combinatin is invalid
                     if False in key:
+                        rejected += 1
                         continue
                     counts[(st_mut.mutation,) + key] += 1
 
